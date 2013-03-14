fe3b73ec55b6d2821d43c9c015938a0718c7a168
realjenius <rjlorimer@realjenius.com>
2013-03-13 21:54:52 -0500

    Initial commit

---
+++ src/main/java/realjenius/hazelcast/RoundRobinSelector.java
@@ -0,0 +1,42 @@
+package realjenius.hazelcast;
+
+import com.hazelcast.core.AtomicNumber;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Member;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <p>The round-robin selector iterates around the Hazelcast cluster, performing a best-guess rotation around the
+ * cluster.</p>
+ * <p>This class uses a shared atomic and consistent member ordering to perform a shared rotation around the elements
+ * of the cluster.</p>
+ * <p>If the cluster changes (a member joins or leaves), it will change the result of the offset operation. This may
+ * result in both a member being skipped on a rotation and in a member being selected twice. For that reason this
+ * should only be used for best-guess balancing, and not for hard equality/fairness.</p>
+ *
+ * @author realjenius/rjlorimer@electrotank.com
+ */
+public class RoundRobinSelector {
+    private final AtomicNumber pivot;
+    private final HazelcastInstance instance;
+
+    public RoundRobinSelector(HazelcastInstance instance, String name) {
+        pivot = instance.getAtomicNumber(name);
+        this.instance = instance;
+    }
+
+    public Member select() {
+        List<Member> members = new ArrayList<>(instance.getCluster().getMembers());
+        int size = members.size();
+        if(size == 0) throw new IllegalStateException("Cluster not connected!");
+        if(size == 1) return members.get(0);
+        // No streaming API in this milestone... sigh.
+        // members.sort(...)
+        Collections.sort(members, (left,right) -> left.getUuid().compareTo(right.getUuid()));
+        return members.get((int)(pivot.incrementAndGet() % size));
+    }
+
+}
---
+++ src/test/java/realjenius/hazelcast/RoundRobinSelectorTest.java
@@ -0,0 +1,49 @@
+package realjenius.hazelcast;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Member;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ *
+ */
+public class RoundRobinSelectorTest {
+
+    @AfterMethod
+    public void shutdown() { Hazelcast.shutdownAll(); }
+
+    @Test
+    public void testRotation() {
+        Set<HazelcastInstance> instances = factory(3, () -> Hazelcast.newHazelcastInstance(null));
+        RoundRobinSelector aSel = new RoundRobinSelector(instances.iterator().next(), "tester");
+        Set<Member> selected = factory(3, () -> aSel.select() );
+        assert 3 == selected.size();
+        assert selected.equals(transform(instances, (i) -> i.getCluster().getLocalMember()));
+    }
+
+    protected <T> Set<T> factory(int times, Supplier<T> supplier) {
+        Set<T> all = new HashSet<>();
+        for(int i=0; i<times; i++) {
+            all.add(supplier.get());
+        }
+        return all;
+    }
+
+    // Current Java 8 build doesn't have transform, and guava shouldn't be needed for this. grumble. c'mon JDK 8!
+    protected <T,E> Set<E> transform(Iterable<T> core, Function<T,E> xfer) {
+        Set<E> conv = new HashSet<>();
+        for(T t : core) {
+            conv.add(xfer.apply(t));
+        }
+        return conv;
+    }
+}
